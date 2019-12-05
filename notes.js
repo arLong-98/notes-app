a172ed1cd97b14a9291d7c5fad667f6f72ae2710
arLong-98 <dhairya999pateriya@gmail.com>
2019-12-06 01:46:32 +0530

    few bugs removed

--- notes.js
+++ notes.js
@@ -2,17 +2,14 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 
-const getNotes = () => {
-    return "your notes...";
-};
 
+//add a note
 const addNotes = (title,body) => {
     const notes = loadNotes();
 
-    const duplicate = notes.find((note) => {
-        note.title === title;
-    });
+    const duplicate = notes.find((note) => note.title === title);
 
+    
     if(!duplicate)
     {
         notes.push({
@@ -29,7 +26,7 @@ const addNotes = (title,body) => {
     }
 };
 
-
+//list all note titles
 const listNotes = ()=>{
     const notes = loadNotes();
     console.log("YOUR NOTES : ");
@@ -38,16 +35,48 @@ const listNotes = ()=>{
     });
     
 }
-
+//display a particular note
 const readNotes =(title)=>{
+    const notes = loadNotes();
+    const found = notes.find((note)=>note.title === title);
 
-}
+    if(found)
+    {
+        console.log(chalk.green.bold(found.title));
+        console.log(found.body);
+    }
+    else
+    {
+        console.log(chalk.red('Note not found !'));
+        
+    }
 
+
+};
+
+
+//delete a particular note
+const removeNotes = (title) => {
+    const notes = loadNotes();
+    //filterArray has notes that are to be kept
+    const filterArray = notes.filter((note) => note.title !== title);
+
+    if(notes.length > filterArray.length)
+    { 
+        console.log(chalk.bgGreen('Note removed !'));
+        saveNotes(filterArray);
+    }
+    else
+        console.log(chalk.bgGreen('No note found !'));
+};
+
+
+//utility function
 const saveNotes = (notes) => {
     const dataJson = JSON.stringify(notes);
     fs.writeFileSync('notes.json',dataJson);
 };
-
+//utility function
 const loadNotes = () => {
     try{
         const dataBuffer = fs.readFileSync('notes.json');
@@ -58,24 +87,15 @@ const loadNotes = () => {
     }
 };
 
-const removeNotes = (title) => {
-    var notes = loadNotes();
-    //filterArray has notes that are to be kept
-    var filterArray = notes.filter((notes) => {
-         notes.title !== title;
-    });
 
-    if(notes.length == filterArray.length)
-        console.log(chalk.bgRed('No note found !'));
-    else
-        console.log(chalk.bgGreen('Note removed !'));
 
-    saveNotes(filterArray);
-};
+
+
+
 
 module.exports = {
-    getNotes : getNotes,
     addNotes : addNotes,
     removeNotes : removeNotes,
-    listNotes : listNotes
+    listNotes : listNotes,
+    readNotes : readNotes
 };
